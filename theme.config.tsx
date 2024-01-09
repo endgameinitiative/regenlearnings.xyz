2e9f880d2fd9e30c88c0ed81c1a95166dc7f1294
Kevin Owocki <kevin@supermodular.xyz>
2024-01-09 11:34:23 -0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>RegenLearnings.xyz</span>,
