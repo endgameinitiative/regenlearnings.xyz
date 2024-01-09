a06bd994250a67b3ece8aee61a657e5e7b51b142
Kevin Owocki <kevin@supermodular.xyz>
2024-01-09 11:32:52 -0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,6 +14,24 @@ const config: DocsThemeConfig = {
     return {
       titleTemplate: '%s – RegenLearnings.xyz'
     }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://regenlearnings.xyz' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'RegenLearnings.xyz'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'RegenLearnings.xyz'}
+        />
+      </>
+    )
   }
 }
 
