ae779abe59c0ead80df9bc5a32d319198f0a3d93
Kevin Owocki <kevin@supermodular.xyz>
2024-01-09 11:35:12 -0700

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
